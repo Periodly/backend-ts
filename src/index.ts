ef3cbed83a9b9022b5f5c7538098f669ce1ff7d3
matt-pasek <mpasek@tm1.edu.pl>
2024-03-02 15:15:21 +0100

    publish friends and mood endpoints

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import sessionRouter from './api/session';
 import userRouter from './api/user';
 import './db';
+import friendsRouter from './api/friends';
+import moodRouter from './api/mood';
 
 dotenv.config();
 
@@ -12,6 +14,8 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use('/api/session', sessionRouter);
 app.use('/api/user', userRouter);
+app.use('/api/mood', moodRouter);
+app.use('/api/friends', friendsRouter);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
