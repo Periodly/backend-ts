83cc84d40c2d8be5d6843b6a4054636e356dfcec
matt-pasek <mpasek@tm1.edu.pl>
2024-03-18 19:58:42 +0100

    documentation for all api/session endpoints

--- src/api/session.ts
+++ src/api/session.ts
@@ -5,6 +5,70 @@ import { login } from '../service/session';
 
 const sessionRouter = express.Router();
 
+/**
+ * @swagger
+ *   tags:
+ *     - name: Authentication
+ *       description: Endpoints for authenticating users.
+ */
+
+/**
+ * @swagger
+ * /api/session/login:
+ *   post:
+ *     description: Logs in a user and retrieves a JSON Web Token (JWT) for authentication.
+ *     tags: [Authentication]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: login
+ *         description: Login credentials
+ *         required: true
+ *         type: object
+ *         properties:
+ *           username:
+ *             type: string
+ *             description: The user's username or email address.
+ *             required: true
+ *           password:
+ *             type: string
+ *             description: The user's password.
+ *             required: true
+ *             minLength: ${minPasswordLength}
+ *     responses:
+ *       200:
+ *         description: Login successful. Response includes a JWT for authentication.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: JWT for authentication.
+ *       400:
+ *         description: Bad request (validation errors, invalid credentials, or other error during login).
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 errors:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *       401:
+ *         description: Unauthorized (invalid credentials).
+ *       500:
+ *         description: Internal server error.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               description: Error message.
+ */
 sessionRouter.post(
   '/login',
   body('username').isString(),
