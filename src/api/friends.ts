678a94dee9c411b75a1bf15a3b21cfe4d9e3dd49
matt-pasek <mpasek@tm1.edu.pl>
2024-03-18 19:31:29 +0100

    documentation for all api/friends endpoints

--- src/api/friends.ts
+++ src/api/friends.ts
@@ -4,7 +4,60 @@ import { addFriend, getFriends } from '../service/friends';
 
 const friendsRouter = express.Router();
 
-// get all friends
+/**
+ * @swagger
+ *   tags:
+ *     - name: Friends
+ *       description: Endpoints for managing friends and friend lists.
+ */
+
+/**
+ * @swagger
+ * definitions:
+ *   Friend:
+ *     type: object
+ *     required:
+ *       - userId
+ *       - friendId
+ *     properties:
+ *       id:
+ *         type: integer
+ *         description: Unique identifier for the friend relationship. (Auto-generated)
+ *       userId:
+ *         type: integer
+ *         description: ID of the user who owns this friend relationship.
+ *       friendId:
+ *         type: integer
+ *         description: ID of the friend in this relationship.
+ */
+
+/**
+ * @swagger
+ * /api/friends/:
+ *   get:
+ *     description: Retrieves a list of friends of the authenticated user.
+ *     security:
+ *       - BearerAuth: []
+ *     tags: [Friends]
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: List of friends.
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/Friend'
+ *       400:
+ *         description: Bad request (validation errors or other error during friend retrieval).
+ *         schema:
+ *           type: object
+ *           properties:
+ *             errors:
+ *               type: array
+ *               items:
+ *                 type: object
+ */
 friendsRouter.get(
   '/',
   header('Authorization').isString().contains('Bearer'),
@@ -21,7 +74,39 @@ friendsRouter.get(
   },
 );
 
-// add a new friend
+/**
+ * @swagger
+ * /api/friends/:
+ *   post:
+ *     description: Adds a new friend to the authenticated user's friend list.
+ *     security:
+ *       - BearerAuth: []
+ *     tags: [Friends]
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: friendName
+ *         description: Name of the friend to add.
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Friend added successfully.
+ *         schema:
+ *           type: array
+ *           items:
+ *              type: string
+ *       400:
+ *         description: Bad request (validation errors, friend already exists, or other error during friend addition).
+ *         schema:
+ *           type: object
+ *           properties:
+ *             errors:
+ *               type: array
+ *               items:
+ *                 type: object
+ */
 friendsRouter.post(
   '/',
   body('friendName').isString(),
