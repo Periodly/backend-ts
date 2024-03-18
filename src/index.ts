1f7c635f1dfeac0ea8d39afefacde4899ccb29ca
matt-pasek <mpasek@tm1.edu.pl>
2024-03-18 19:15:37 +0100

    initialize documentation

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,32 @@ import userRouter from './api/user';
 import './db';
 import friendsRouter from './api/friends';
 import moodRouter from './api/mood';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'PRDLY-lite API',
+      version: '0.0.2',
+    },
+  },
+  apis: ['./src/api/*.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 app.use(express.json());
 app.use('/api/session', sessionRouter);
 app.use('/api/user', userRouter);
 app.use('/api/mood', moodRouter);
 app.use('/api/friends', friendsRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
